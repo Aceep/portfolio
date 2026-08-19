4c984b1f4f8812ac19720818df14e2ef1edbabe3
Alycia Gautier <108153485+Aceep@users.noreply.github.com>
2026-08-19 16:41:34 +0200

    nettoyage : eslint commité, code mort supprimé, fix skills + filtres

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    // The provider and its consumer hook belong in the same module; splitting
+    // them purely to satisfy Fast Refresh would cost a file and buy nothing.
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true, allowExportNames: ['useLanguage'] },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+};
